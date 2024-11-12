c60eae8856afb8ee13fa6dc6f71be0ed0bcd2352
GuiRomanholi <guilhermeroma343@gmail.com>
2024-11-12 17:07:00 -0300

    Boyler Plate Limpo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
 
 export const metadata: Metadata = {
@@ -11,7 +13,9 @@ export default function RootLayout({children,}: Readonly<{  children: React.Reac
   return (
     <html lang="pt-br">
       <body>
+        <Header/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
