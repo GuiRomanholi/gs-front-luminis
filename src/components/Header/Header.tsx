499d1fda5686f757d8cd25b023d780d10ad80059
GuiRomanholi <guilhermeroma343@gmail.com>
2024-11-12 19:21:21 -0300

    tinah esquecido do Home

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,6 +13,9 @@ export default function Header() {
         </div>
         <div>
           <ul className="flex space-x-6 text-white">
+          <li>
+              <Link href="/">Home</Link>
+            </li>
             <li>
               <Link href="/">Quem somos</Link>
             </li>
