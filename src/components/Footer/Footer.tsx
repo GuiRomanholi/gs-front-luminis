1f52ad982656c859472f87b39fce029b77647630
GuiRomanholi <guilhermeroma343@gmail.com>
2024-11-12 17:34:25 -0300

    Cabecalho bonito

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,36 @@
+import Link from 'next/link'
 import React from 'react'
+import Image from "next/image";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+        <div>
+            <ul>
+                <li>
+                    <Link href="/">
+                        {<Image src="/img/logo_luminus.png" alt="Logo_footer" width={200} height={200}/>}
+                    </Link>
+                </li>
+                <p>|</p>
+                <li>
+                    <a href="https://github.com/GuiRomanholi/gs-front-luminis" target="_blank">
+                        {<Image src="/img/logo_github_branco.png" alt="Youtube_Branco" width={200} height={200}/>}
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/GuiRomanholi/gs-front-luminis" target="_blank">
+                        {<Image src="/img/logo_github_branco.png" alt="Github_Branco" width={200} height={200}/>}
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/GuiRomanholi/gs-front-luminis" target="_blank">
+                        {<Image src="/img/logo_github_branco.png" alt="Instagram_Branco" width={200} height={200}/>}
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <p>&copy; 2024 Luminis. Todos os direitos reservados</p>
+    </footer>
   )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from 'react';
+import Image from "next/image";
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="bg-[#333333]">
+      <nav className="flex items-center justify-between px-8 py-4">
+        <div className="flex-shrink-0">
+          <Link href="/">
+            <Image src="/img/logo_luminus.png" alt="Logo Luminus" width={150} height={40} className="w-32 h-auto" />
+          </Link>
+        </div>       
+        <div>
+          <ul className="flex space-x-6 text-white">
+          <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/">Quem somos</Link>
+            </li>
+            <li>
+              <Link href="/">Fale conosco</Link>
+            </li>
+            <li>
+              <Link href="/">FAQ</Link>
+            </li>
+            <li>
+              <Link href="/">Iniciar Sessão</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 }
