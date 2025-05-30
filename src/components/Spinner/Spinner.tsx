c23f88b418a7d3b1518f29c1bc6999589a3e8e0b
GuiRomanholi <guilhermeroma343@gmail.com>
2025-05-30 08:02:50 -0300

    a

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
 
 export default function Spinner() {
   return (
     <div className="spin-div">
-        <div className="spin-div2"></div>
+      <div className="spin-div2"></div>
     </div>
   )
 }
+//aaaaaaaaaaaaaaaaaaaaaa
+
+
+
+
+
+
+
+
